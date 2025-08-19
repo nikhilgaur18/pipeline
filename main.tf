347271bfcd00677420cd71af302e5f7598b99184
nikhil <nikhil>
2025-08-19 10:25:02 +0000

    main.tf prepared for ist ec2

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_security_group" "my_server" {
+name = "myserver-sg"
+description = "tcp port config"
+ingress {
+description = "all incoming traffic"
+from_port = 22
+to_port = 22
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+description = "outbound traffic"
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_instance" "pipline-ec2" {
+ami = "ami-013e72d4d2950004e"
+instance_type = "t3.medium"
+vpc_security_group_ids = [aws_security_group.my_server.id]
+key_name = "key.pam"
+}
+
+output "ec2_public_ip" {
+value = aws_instance.pipline-ec2.public_ip
+}
