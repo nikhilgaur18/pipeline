19004de9f3ed25bd7f36a451a3bec94dfbcfa26a
nikhilgaur18 <nikhilgaur18@gmail.com>
2025-08-19 18:55:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket = "websitebucket-niks"   # <-- your existing bucket
+    key    = "terraform/jenkins.tfstate"
+    region = "us-east-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
 region = "us-east-1"
 }
